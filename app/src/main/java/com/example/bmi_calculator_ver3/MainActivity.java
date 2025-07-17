48e326f13209acb7dac96483a4bf4543ef378012
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-07-17 19:46:37 +0200

    commited

--- app/src/main/java/com/example/bmi_calculator_ver3/MainActivity.java
+++ app/src/main/java/com/example/bmi_calculator_ver3/MainActivity.java
@@ -2,20 +2,26 @@ package com.example.bmi_calculator_ver3;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.bmi_calculator_ver3.impl.CalcLogic_impl;
 import com.example.bmi_calculator_ver3.impl.DisplayController_impl;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textViewBMI, textViewComment;
+    private EditText editTextHeight, editTextWeight;
+    private CheckBox checkBox;
     private Button button;
     private ImageButton imageButton;
+    private DrawerLayout drawerLayout;
     private LinearLayout textAboutApp, textBMI, textClassification;
 
     private CalcLogic calcLogic;
