f47a5206c5e72ce96825a4063cbb0ba79b8d0128
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-07-17 19:32:54 +0200

    commited

--- app/src/main/java/com/example/bmi_calculator_ver3/MainActivity.java
+++ app/src/main/java/com/example/bmi_calculator_ver3/MainActivity.java
@@ -1,17 +1,32 @@
 package com.example.bmi_calculator_ver3;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.bmi_calculator_ver3.impl.CalcLogic_impl;
+import com.example.bmi_calculator_ver3.impl.DisplayController_impl;
+
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private Button button;
+    private ImageButton imageButton;
+
+    private CalcLogic calcLogic;
+    private DisplayController displayController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.caclculate_page);
+
+        calcLogic = new CalcLogic_impl();
+        displayController = new DisplayController_impl(calcLogic);
 
     }
 }
