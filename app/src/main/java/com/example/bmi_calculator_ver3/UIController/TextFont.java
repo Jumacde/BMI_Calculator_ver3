cab4b2a778a7f4bf6ae4f9c3f06b5ad7b6b29003
Yuma Takamura <s1729686@stud.uni-frankfurt.de>
2025-07-19 20:49:43 +0200

    commited

--- app/src/main/java/com/example/bmi_calculator_ver3/UIController/TextFont.java
+++ app/src/main/java/com/example/bmi_calculator_ver3/UIController/TextFont.java
@@ -1,4 +1,7 @@
 package com.example.bmi_calculator_ver3.UIController;
 
 public interface TextFont {
+    String getText();
+
+    void setText(String text);
 }
--- app/src/main/java/com/example/bmi_calculator_ver3/UIController/impl/TextFont_impl.java
+++ app/src/main/java/com/example/bmi_calculator_ver3/UIController/impl/TextFont_impl.java
@@ -3,9 +3,19 @@ package com.example.bmi_calculator_ver3.UIController.impl;
 import com.example.bmi_calculator_ver3.UIController.TextFont;
 
 public class TextFont_impl implements TextFont {
+    String text;
 
-    public TextFont_impl() {
-
+    public TextFont_impl(String text) {
+        this.text = text;
     }
 
+    @Override
+    public String getText() {
+        return text;
+    }
+
+    @Override
+    public void setText(String text) {
+        this.text = text;
+    }
 }
